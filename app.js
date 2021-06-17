99c228e334f75e6ccf18028fd5f524f6efddd37b
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-06-17 16:43:24 -0400

    Add protection middleware

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 require('dotenv').config();
+const hpp = require('hpp');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(hpp());
+app.use(xss());
 
 const PORT = process.env.PORT || 5000;
 
