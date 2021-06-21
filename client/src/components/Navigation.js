c4e39e517f81ba0a80a4fa27bcb667237199380b
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-06-21 09:08:19 -0400

    Translate the Homepage

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -24,6 +24,7 @@ const Navigation = () => {
 
   const changeLanguage = (lang) => {
     i18n.changeLanguage(lang);
+    clickHandler();
   };
 
   return (
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,30 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import './Home.css';
 import homeP from '../images/home.jpg';
 import Meta from '../components/Meta';
 
 const Home = () => {
+  const [t, i18n] = useTranslation();
   return (
-    <Fragment>
-      <Meta title='Accueil' />
-      <section className='home'>
-        <img src={homeP} alt='tichif' className='home-image' />
-        <div className='home-content'>
-          <h3>Dalzon Charles-Hébert</h3>
-          <small>Fullstack web developer</small>
-          <p>
-            Un développeur web à la recherche de l'opportunité d'apporter ses
-            compétences et de les appliquer dans un environnement de travail qui
-            l'encouragera à progresser et à acquérir de nouvelles connaissances,
-            dans l'intention d'aider l'entreprise à atteindre ses objectifs.
-          </p>
-          <button className='btn'>
-            <i className='fas fa-download'></i> Télécharger CV
-          </button>
-        </div>
-      </section>
-    </Fragment>
+    <Suspense fallback='loading'>
+      <Fragment>
+        <Meta title='Accueil' />
+        <section className='home'>
+          <img src={homeP} alt='tichif' className='home-image' />
+          <div className='home-content'>
+            <h3>Dalzon Charles-Hébert</h3>
+            <small>Fullstack web developer</small>
+            <p>{t('home.desc')}</p>
+            <button className='btn'>
+              <i className='fas fa-download'></i> {t('home.button')}
+            </button>
+          </div>
+        </section>
+      </Fragment>
+    </Suspense>
   );
 };
 
