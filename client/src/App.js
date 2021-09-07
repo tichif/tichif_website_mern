da9eee1e0e1da7ccebe407ef85c724eec3ae0fc2
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-09-07 12:07:14 -0400

    Use react.loazy for splitting code

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './pages/Home';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import Work from './pages/Work';
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/services' component={Services} />
-        <Route exact path='/contact' component={Contact} />
-        <Route exact path='/works' component={Work} />
-      </Switch>
-    </Router>
+    <Suspense fallback='loading'>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route
+            exact
+            path='/services'
+            component={lazy(() => import('./pages/Services'))}
+          />
+          <Route
+            exact
+            path='/contact'
+            component={lazy(() => import('./pages/Contact'))}
+          />
+          <Route
+            exact
+            path='/works'
+            component={lazy(() => import('./pages/Work'))}
+          />
+        </Switch>
+      </Router>
+    </Suspense>
   );
 };
 
